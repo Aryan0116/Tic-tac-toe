1bb1fd436530df8d61ca8aa90be9740e199c0b82
Aryan Singh <108578411+Aryan0116@users.noreply.github.com>
2024-11-03 01:17:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://aryan0116.github.io'
+}));
 
 const rooms = new Map();
 
